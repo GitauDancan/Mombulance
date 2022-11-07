4cfd82c84d1e6e7e78696b517d4dfa832624bc65
GitauDancan <dangitau2000@gmail.com>
2022-11-07 18:00:59 +0300

    sql authentication

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const sessions = require('express-session');
+const session = require('express-session');
 const cookieParser = require("cookie-parser");
 const IndexRoutes = require('./src/routes/index');
 const path = require('path');
@@ -11,11 +11,11 @@ const oneDay = 1000 * 60 * 60 * 24;
 
 // cookie parser middleware
 app.use(cookieParser());
-app.use(sessions({
+app.use(session({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    saveUninitialized:true,
+    saveUninitialized: true,
     cookie: { maxAge: oneDay },
-    resave: false 
+    resave: false
 }));
 // parsing the incoming data
 app.use(express.json());
@@ -24,13 +24,8 @@ app.use(bodyParser.urlencoded({ extended: true, useNewUrlParser: true }));
 //serving public file
 app.use(express.static("src"));
 
-//username and password
-const myusername = 'user1'
-const mypassword = 'mypassword'
-  
-
 // a variable to save a session
-var session;
+// var session;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './src/views'));
--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const Port = process.env.NODE_ENV === 'test' ? 4001 : (process.env.PORT || 1333);
+const Port = process.env.NODE_ENV === 'test' ? 4001 : (process.env.PORT || 3306);
 
 module.exports = {
   Port: Port,
